bef02f7c30a37835beffeaf5ec30b9dfa67a5e99
sjchatterjee73 <soumyajitchatterjee73@gmail.com>
2018-11-01 16:52:43 +0530

    Delete From a List

---
+++ DeleteFromList.c
@@ -0,0 +1,53 @@
+/*
+You're given a list of Integer Items. You have to compare every item with 'K', if it is less than 'K' you have to remove the item from the list, and return the modified list.
+
+Examples:
+<pre>
+Input : list: 4,5,1,3,5,7,9,3           k: 5
+Output : 5,5,7,9
+Explanation: Those item whose value is less than k, is removed from the list.
+
+Approach:
+1. change the head, if the head itself contains lower value than 'k'.
+    1.1 traverse the loop until we are getting one value which is greater than 'k'.
+    1.2. unlink the current 'head' from the original 'head'
+2. erase/unlink those value from the list whose are contains lower value than 'k'.
+3. return the head;
+
+//Asked In: Microsoft
+*/
+
+struct node * fun(int k, struct node * head){
+	if((head) == NULL)
+		return NULL;
+	
+	if((head)->data < k && (head)->next == NULL)
+		return NULL;
+	
+	struct node * temp = head;
+	struct node * current = NULL;
+	
+	// change the head, if head itself contains lower value than k.
+	// also traverse upto end, for deleting last node if it is less than k.
+	while(temp != NULL && temp->data < k){
+		current = temp;
+		temp = temp->next;
+	}
+	
+	// delete the last node if it is less than k.
+	if(current != NULL)
+		current->next = NULL;
+	
+	struct node * newHead = temp;
+	// erase the link of those node, from the list, whose having less value than k.
+	while(temp->next != NULL){
+		if(temp->next->data < k){
+			temp->next = temp->next->next;
+		}else{
+			temp = temp->next;
+		}
+	}
+	free(head);
+	head = newHead;
+	return head;
+}
